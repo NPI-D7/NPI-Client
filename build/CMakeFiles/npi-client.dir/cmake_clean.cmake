bd355cd7995cd8f15ba3e591e4d4d0567593c554
Tobi-D7 <tobid7vx@gmail.com>
2021-12-23 20:09:05 +0000

    Initial Commit

---
+++ build/CMakeFiles/npi-client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/npi-client.dir/src/main.cpp.o"
+  "CMakeFiles/npi-client.dir/src/main.cpp.o.d"
+  "npi-client"
+  "npi-client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/npi-client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
